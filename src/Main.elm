e199ddc3dd4854b2a94b20f0f5a56ecf200f5949
Travis Shears <btbtravis@gmail.com>
2019-10-15 21:35:39 +0200

    Style dropdowns

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,15 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
+--import Html.Extra as Html
+
 import Browser
 import Debug exposing (log)
 import Html exposing (Html, div, h1, i, option, p, select, text)
 import Html.Attributes exposing (class, selected, value)
 import Html.Events exposing (onInput)
 import List exposing (map)
+import Svg exposing (path, svg)
+import Svg.Attributes exposing (d, fill, height, viewBox, width)
 
 
 
@@ -33,7 +37,7 @@ grades =
     , { french = "2", uiaa = "2", yds = "5.1/5.2", skill = "Novice" }
     , { french = "3", uiaa = "3", yds = "5.3/5.4", skill = "Novice" }
     , { french = "4a", uiaa = "4", yds = "5.5", skill = "Beginner" }
-    , { french = "4b", uiaa = "4++", yds = "5.6", skill = "Beginner" }
+    , { french = "4b", uiaa = "4+", yds = "5.6", skill = "Beginner" }
     , { french = "4c", uiaa = "5", yds = "5.7", skill = "Beginner" }
     , { french = "5a", uiaa = "5+", yds = "5.8", skill = "Beginner" }
     , { french = "5b", uiaa = "6-", yds = "5.9", skill = "Intermediate" }
@@ -73,7 +77,7 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { currentGrade = 2 }, Cmd.none )
+    ( { currentGrade = 8 }, Cmd.none )
 
 
 
@@ -92,13 +96,18 @@ update msg model =
 
 
 
---onChange : (String -> msg) -> Attribute msg
---onChange handler =
---on "change" <| Json.map handler <| Json.at [ "target", "value" ] Json.string
---[ onChange (\s -> SetCurrentGrade (Maybe.withDefault 0 (String.toInt s)))
---( model, Cmd.none )
---[ on "change" (Json.at [ "target", "value" ] Json.string) handleSetCurrentGrade
 ---- VIEW ----
+
+
+viewDownArrowSvg : Html Msg
+viewDownArrowSvg =
+    svg [ Svg.Attributes.class "down-arrow", width "29", height " 20", viewBox "0 0 29 20", fill "none" ]
+        [ path
+            [ d "M18.0791 18.3311C16.1169 20.3425 12.8831 20.3425 10.9209 18.3311L1.566 8.74146C-1.52375 5.57419 0.720343 0.25 5.14506 0.250001L23.855 0.250002C28.2797 0.250003 30.5238 5.57419 27.434 8.74146L18.0791 18.3311Z"
+            , fill "#4E4B5C"
+            ]
+            []
+        ]
 
 
 handleSetCurrentGrade : String -> Msg
@@ -108,25 +117,28 @@ handleSetCurrentGrade s =
 
 viewDropdown : String -> List String -> Model -> Html Msg
 viewDropdown title os model =
-    div []
-        [ p [] [ text title ]
-        , select [ onInput handleSetCurrentGrade ]
-            (List.indexedMap
-                (\i o ->
-                    option
-                        [ selected (i == model.currentGrade)
-                        , value (String.fromInt i)
-                        ]
-                        [ text o ]
+    div [ class "dropdown" ]
+        [ p [ class "dropdown__label" ] [ text title ]
+        , div [ class "select__wrapper" ]
+            [ select [ onInput handleSetCurrentGrade ]
+                (List.indexedMap
+                    (\i o ->
+                        option
+                            [ selected (i == model.currentGrade)
+                            , value (String.fromInt i)
+                            ]
+                            [ text o ]
+                    )
+                    os
                 )
-                os
-            )
+            , viewDownArrowSvg
+            ]
         ]
 
 
 viewDropDowns : Model -> Html Msg
 viewDropDowns model =
-    div []
+    div [ class "dropdowns" ]
         [ viewDropdown "French" (singleGrade French) model
         , viewDropdown "UIAA" (singleGrade UIAA) model
         , viewDropdown "YDS" (singleGrade YDS) model
@@ -137,7 +149,7 @@ viewDropDowns model =
 viewHeader : Html Msg
 viewHeader =
     div [ class "header" ]
-        [ h1 [ class "title" ] [ text """Climbing 
+        [ h1 [ class "title" ] [ text """Climbing
 Grade
 Conversion
 Tool
