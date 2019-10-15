ea65db19fe9608aa802fcadd3aac39d2385cdf7e
Travis Shears <btbtravis@gmail.com>
2019-10-15 15:37:25 +0200

    make all dropdowns update when one changes

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
+import Debug exposing (log)
 import Html exposing (Html, div, h1, i, option, p, select, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, selected, value)
+import Html.Events exposing (onInput)
 import List exposing (map)
 
 
@@ -27,16 +29,24 @@ type alias Grade =
 
 grades : List Grade
 grades =
-    [ { french = "1"
-      , uiaa = "1"
-      , yds = "5"
-      , skill = "Novice"
-      }
-    , { french = "2"
-      , uiaa = "2"
-      , yds = "5.1/5.2"
-      , skill = "Novice"
-      }
+    [ { french = "1", uiaa = "1", yds = "5", skill = "Novice" }
+    , { french = "2", uiaa = "2", yds = "5.1/5.2", skill = "Novice" }
+    , { french = "3", uiaa = "3", yds = "5.3/5.4", skill = "Novice" }
+    , { french = "4a", uiaa = "4", yds = "5.5", skill = "Beginner" }
+    , { french = "4b", uiaa = "4++", yds = "5.6", skill = "Beginner" }
+    , { french = "4c", uiaa = "5", yds = "5.7", skill = "Beginner" }
+    , { french = "5a", uiaa = "5+", yds = "5.8", skill = "Beginner" }
+    , { french = "5b", uiaa = "6-", yds = "5.9", skill = "Intermediate" }
+    , { french = "5c", uiaa = "6", yds = "5.10a", skill = "Intermediate" }
+    , { french = "6a", uiaa = "6+", yds = "5.10b", skill = "Intermediate" }
+    , { french = "6a+", uiaa = "7-", yds = "5.10c", skill = "Intermediate" }
+    , { french = "6b", uiaa = "7", yds = "5.10d", skill = "Intermediate" }
+    , { french = "6b+", uiaa = "7+", yds = "5.11a", skill = "Advanced" }
+    , { french = "6c", uiaa = "8-", yds = "5.11b", skill = "Advanced" }
+    , { french = "6c+", uiaa = "8", yds = "5.11c", skill = "Advanced" }
+    , { french = "7a", uiaa = "8+", yds = "5.11d", skill = "Advanced" }
+    , { french = "7a+", uiaa = "9-", yds = "5.12a", skill = "Advanced" }
+    , { french = "7b", uiaa = "9-/9", yds = "5.12b", skill = "Expert" }
     ]
 
 
@@ -57,12 +67,13 @@ singleGrade g =
 
 
 type alias Model =
-    {}
+    { currentGrade : Int
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { currentGrade = 2 }, Cmd.none )
 
 
 
@@ -70,33 +81,56 @@ init =
 
 
 type Msg
-    = NoOp
+    = SetCurrentGrade Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SetCurrentGrade i ->
+            ( { model | currentGrade = i }, Cmd.none )
 
 
 
+--onChange : (String -> msg) -> Attribute msg
+--onChange handler =
+--on "change" <| Json.map handler <| Json.at [ "target", "value" ] Json.string
+--[ onChange (\s -> SetCurrentGrade (Maybe.withDefault 0 (String.toInt s)))
+--( model, Cmd.none )
+--[ on "change" (Json.at [ "target", "value" ] Json.string) handleSetCurrentGrade
 ---- VIEW ----
 
 
-viewDropdown : String -> List String -> Html Msg
-viewDropdown title os =
+handleSetCurrentGrade : String -> Msg
+handleSetCurrentGrade s =
+    SetCurrentGrade (Maybe.withDefault 0 (String.toInt s))
+
+
+viewDropdown : String -> List String -> Model -> Html Msg
+viewDropdown title os model =
     div []
         [ p [] [ text title ]
-        , select []
-            (map (\o -> option [] [ text o ]) os)
+        , select [ onInput handleSetCurrentGrade ]
+            (List.indexedMap
+                (\i o ->
+                    option
+                        [ selected (i == model.currentGrade)
+                        , value (String.fromInt i)
+                        ]
+                        [ text o ]
+                )
+                os
+            )
         ]
 
 
-viewDropDowns : Html Msg
-viewDropDowns =
+viewDropDowns : Model -> Html Msg
+viewDropDowns model =
     div []
-        [ viewDropdown "French" (singleGrade French)
-        , viewDropdown "UIAA" (singleGrade UIAA)
-        , viewDropdown "YDS" (singleGrade YDS)
+        [ viewDropdown "French" (singleGrade French) model
+        , viewDropdown "UIAA" (singleGrade UIAA) model
+        , viewDropdown "YDS" (singleGrade YDS) model
+        , viewDropdown "Basic Skill" (singleGrade Skill) model
         ]
 
 
@@ -116,7 +150,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ viewHeader
-        , viewDropDowns
+        , viewDropDowns model
         ]
 
 
